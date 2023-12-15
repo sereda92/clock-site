910bd002d7c6c40a3deecf0f02eb41e0970c1fc3
sereda92 <assakay92@gmail.com>
2023-12-15 14:29:38 +0200

    edition-15/12

--- js/main.js
+++ js/main.js
@@ -1,24 +1,20 @@
 const anchors = document.querySelectorAll('a[href*="#"]')
 
 for (let anchor of anchors){
-   anchor.addEventListener("click", function(event){
-      event.preventDefault();
-      const blockID = anchor.getAttribute('href')
-      document.querySelector('' + blockID).scrollIntoView({
-         behavior: "smooth",
-         block: "start"
-      })
-   })
+	anchor.addEventListener("click", function(event){
+    event.preventDefault();
+    const blockID = anchor.getAttribute('href')
+    document.querySelector('' + blockID).scrollIntoView({
+    behavior: "smooth",
+    block: "start"
+    })
+})
 }
 
-
-// -----------------------------------------------------------------
-// --------------------ФИКСИРОВАННЫЙ HEADER-------------------------
-// -----------------------------------------------------------------
+// --------------------FIXED-HEADER-------------------------
 const header = document.querySelector('.header__menu-background ');
 const first = document.querySelector('.header__item-contant');
-const btnArrow = document.querySelector('.btn-arrow');
-const arrow = document.querySelector('.arrow');
+const headerLink = document.querySelectorAll('.header__link');
 
 const headerHeight = header.offsetHeight;
 const firstHeight = first.offsetHeight;
@@ -26,32 +22,32 @@ let lastScrollTop = 0;
 
 window.addEventListener('scroll', () => {
 	let scrollDistance = window.scrollY;
-
 	if (scrollDistance >= firstHeight + headerHeight) {
 		header.classList.add('header--fixed');
-        arrow.classList.add('arrow-fixed')
-		// first.style.marginTop = `${headerHeight}px`;
+		headerLink.forEach(link => link.classList.add('header__link-fixed'));
 	} else {
 		header.classList.remove('header--fixed');
-		// first.style.marginTop = null;
-        arrow.classList.add('arrow')
+		headerLink.forEach(link => link.classList.remove('header__link-fixed'));
 	}
-
-	// if (scrollDistance > lastScrollTop) {
-		// header.classList.remove('header--fixed');
-		// arrow.classList.remove('arrow-fixed')
-		// first.style.marginTop = null;
-	// } else {
-		// header.classList.add('header--fixed');
-		// arrow.classList.add('arrow-fixed')
-		// first.style.marginTop = `${headerHeight}px`;
-	// }
-
 	if (scrollDistance === 0) {
 		header.classList.remove('header--fixed');
-        arrow.classList.remove('arrow-fixed')
+		headerLink.forEach(link => link.classList.remove('header__link-fixed'));
 		first.style.marginTop = null;
 	}
-
 	lastScrollTop = scrollDistance;
 });
+
+
+// ----------------------search----
+
+const searchBtn = document.getElementById('search_btn')
+const searchInp = document.getElementById('search_inp')
+
+
+searchBtn.addEventListener('click', function(){
+	if(searchInp.classList.contains('search__active')){
+		searchInp.classList.remove('search__active');
+	}else{
+		searchInp.classList.add('search__active')
+	}
+})
